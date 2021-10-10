99a7cade968fa1ef2012828d19b46e3cb3f086c8
Muhammad Rizqi Fakhri Kurniawan <rizqifakhri@students.unnes.ac.id>
2021-10-10 16:24:41 +0700

    Problem_04

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,25 +6,61 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody2D rigidBody2D;
     public float speed;
+    private bool isPressed = false;
     public float currentSpeed;
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        PushBall();
     }
-    
-    //Rumusan untuk gerakan bola
+
     private void PushBall()
     {
-        var xForce = Random.Range(-1f, 1f);
-        var yForce = Random.Range(-1f, 1f);
-        var force = new Vector2(xForce, yForce);
-        rigidBody2D.velocity = force.normalized * speed;
+        if (isPressed) return;
+
+        var force = new Vector2(0f, 0f);
+        
+        // Arah bola bisa ditekan "W" bola atas
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            var north = new Vector2(0f, 1f);
+            force = north;
+            isPressed = true;
+        }
+
+        // Arah bola bisa ditekan "D" bola kanan
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            var east = new Vector2(1f, 0f);
+            force = east;
+            isPressed = true;
+        }
+
+        // Arah bola bisa ditekan "S" bola bawah
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            var south = new Vector2(0f, -1f);
+            force = south;
+            isPressed = true;
+        }
+
+        // Arah bola bisa ditekan ""A bola kiri
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            var west = new Vector2(-1f, 0f);
+            force = west;
+            isPressed = true;
+        }
+
+        if (isPressed)
+        {
+            rigidBody2D.velocity = force.normalized * speed;
+        }
     }
 
     private void Update()
     {
         currentSpeed = rigidBody2D.velocity.magnitude;
+        PushBall();
     }
 }
