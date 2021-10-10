1c9f56efc3fe9ef149f0c24ea5acdedb805ce671
Muhammad Rizqi Fakhri Kurniawan <rizqifakhri@students.unnes.ac.id>
2021-10-10 21:52:17 +0700

    Problem 10

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button[] btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < btn.Length; i++)
+        {
+            int index = i + 1;
+            btn[i].onClick.AddListener(() => { SceneManager.LoadScene(index); });
+        }
+    }
+
+    public void OnClickMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
