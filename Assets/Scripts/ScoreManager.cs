1511efe2c1ca3cb00bf96520be4fda64d16ec6bc
Muhammad Rizqi Fakhri Kurniawan <rizqifakhri@students.unnes.ac.id>
2021-10-10 19:18:47 +0700

    Problem 7

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//Script score manager
+public class ScoreManager : MonoBehaviour
+{
+    // Deklarasi Scoretext dan Player
+    public Text scoreText;
+    private Ball player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Ball>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + player.currentScore.ToString();
+    }
+}
